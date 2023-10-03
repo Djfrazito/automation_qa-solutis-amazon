726f5c3460fb08c2dc12240c7c2470b21ae73f72
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-10-03 11:09:50 -0300

    feat: adicionando casos de teste do carrinho( adicionar ao carrinho)

---
+++ cypress/e2e/pucharsing-journey-cart.cy.js
@@ -0,0 +1,64 @@
+const productAsin = require('../fixtures/productAsin.json')
+let quantity = '';
+
+beforeEach(() => {
+    // Amazon Standard Identification Number
+    cy.visit(`/dp/${productAsin.echoDot}`)
+});
+
+describe('Página de produto adicionar ao carrinho', () => {
+
+    it('Valida botão de adicionar ao carrinho na PDP', () => {
+        cy.get('#add-to-cart-button').should('be.visible');
+            }); 
+
+    it('Deve adicionar o produto ao carrinho', () => {
+        
+        cy.addToCartAndCloseRecommendation();
+
+        cy.waitForAutoComplete();
+    
+        // verifica se o número de itens no cart corresponde a 1
+        cy.get('#nav-cart-count').should('be.visible').invoke('text').then((text) => {
+            expect(text.trim()).to.eq('1');
+        });
+    });
+
+    it('Deve adicionar uma quantidade do produto ao carrinho pela PDP', () => {
+        quantity = '2';
+
+        cy.get('#quantity')
+        .should('be.visible')
+        .select(`${quantity}`);
+        
+        cy.addToCartAndCloseRecommendation();
+
+        cy.waitForAutoComplete();
+    
+        // verifica se o número de itens no cart corresponde à quantidade a quantidade selecionada
+        cy.get('#nav-cart-count').should('be.visible').invoke('text')
+        .then((text) => {
+            expect(text.trim()).to.eq(quantity);
+        });
+    });
+    
+    it('Deve cancelar a adição de itens pelo menu lateral do carrinho', () => {
+
+        cy.addToCartAndCloseRecommendation();
+
+        cy.waitForAutoComplete();
+
+        cy.get('input[title="Excluir"]').trigger('click');
+
+        cy.get('[data-csa-c-func-deps="aui-da-ewc-delete-item"]').should('not.exist');
+        cy.get('#nav-cart-count').should('be.visible').click();
+
+        cy.waitForAutoComplete();
+
+        cy.get('#nav-cart-count').invoke('text').should('be.visible')
+        .then((text) => {
+            expect(text.trim()).to.eq('0');
+        });
+    });
+    
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,65 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * Login na conta do usuário.
+ * {String} email
+ * {String} password
+ */
+Cypress.Commands.add('login', (email, password) => {
+    cy.session([email, password], () => {
+      cy.visit("/")
+      // Verificando se o navbar existe.
+      cy.get('#navbar-main') 
+      .should('exist')
+  
+      // Abrindo o menu da navbar e clicando em login.
+      cy.get("#nav-link-accountList").trigger('mouseover');
+      cy.get("#nav-flyout-accountList").should('be.visible');
+      cy.get('a[data-nav-ref="nav_signin"]').click();
+  
+      // Login com variáveis de ambiente (cypress.env).
+      cy.get('#ap_email').should('be.visible').and('have.value', '').type(email, {log: false});
+      cy.get('.a-button-input').click();
+      cy.get('#ap_password').should('be.visible').and('have.value', '').type(`${password}{enter}`, {log: false});
+    }, {cacheAcrossSpecs: true})
+  });
+
+Cypress.Commands.add('addToCartAndCloseRecommendation', () =>{
+    // fecha a tela de recomendação se ela for exibida
+    cy.get('#add-to-cart-button').should('be.visible').click();
+    cy.get('body').then(($body) => {
+        if ($body.find('.a-popover-header > .a-button-close').length > 0) {   // verifica se o botão existe no corpo
+            cy.get('.a-button-close')
+                .should('be.visible')
+                .click();
+        }
+    });
+});
+
+Cypress.Commands.add('waitForAutocomplete', () => {
+    // aguarda a proxima requisição de atualização de UI
+    cy.intercept('POST', '**AutocompleteUIServiceMetrics**').as('getCart');
+    cy.wait('@getCart');
+});
+
+Cypress.Commands.add('addProductToCart', (productAsin) => {
+    // Visitar a página do produto
+    cy.visit(`/dp/${productAsin}`);
+  
+    // Adicionar o produto ao carrinho e fechar a recomendação
+    cy.addToCartAndCloseRecommendation();
+  
+    // Ir para o carrinho
+    cy.get('.a-spacing-base > .a-button > .a-button-inner > .a-button-text')
+      .should('be.visible')
+      .click();
+  });
+  
+  Cypress.Commands.add('removeProductFromCart', (productAsin) => {
+    cy.visit('/cart')
+    // Encontrar o produto pelo ASIN e clicar no link 'Excluir'
+    cy.get(`[data-asin="${productAsin}"]`)
+      .find('.sc-action-delete > .a-declarative > .a-color-link')
+      .contains('Excluir')
+      .click();
+  });
+  
+  
