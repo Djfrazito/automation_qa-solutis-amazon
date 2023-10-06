de706be630a06dc956f2104c40e9ed2c379ac1dd
C98605A <@Dj11219>
2023-10-06 17:46:09 -0300

    Teste execução com variaveis de ambiente

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,7 +4,7 @@
  * {String} password
  */
 Cypress.Commands.add('login', (email, password) => {
-  cy.session([email, password], () => {
+  cy.session(email, () => {
     cy.visit("/")
     // Verificando se o navbar existe.
     cy.get('#navbar-main') 
@@ -18,9 +18,13 @@ Cypress.Commands.add('login', (email, password) => {
     // Login com variáveis de ambiente (cypress.env).
     cy.get('#ap_email').type(email, {log: false});
     cy.get('.a-button-input').click();
+    cy.intercept('GET', 'https://www.amazon.com/aaut/verify/**', {
+      query: {
+        options: '%7B%22clientData%22%3A%22%7B%5C%22sessionId%5C%22%3A%5C%22133-3733073-8219722%5C%22%2C%5C%22marketplaceId%5C%22%3A%5C%22A2Q3Y263D00KWC%5C%22%2C%5C%22rid%5C%22%3A%5C%220Q5T4J51SW9B51YDSQ7X%5C%22%2C%5C%22ubid%5C%22%3A%5C%22133-9706136-6813011%5C%22%2C%5C%22pageType%5C%22%3A%5C%22AuthenticationPortal%5C%22%2C%5C%22appAction%5C%22%3A%5C%22SIGNIN_PWD_COLLECT%5C%22%2C%5C%22subPageType%5C%22%3A%5C%22SignInClaimCollect%5C%22%7D%22%2C%22challengeType%22%3Anull%2C%22locale%22%3A%22pt-BR%22%2C%22enableHeaderFooter%22%3Atrue%2C%22enableBypassMechanism%22%3Afalse%2C%22enableModalView%22%3Afalse%2C%22eventTrigger%22%3A%22PageLoad%22%7D'
+      }
+    })
     cy.get('#ap_password').type(`${password}{enter}`, {log: false});
   }, {
-    cacheAcrossSpecs: true, 
     validate: () => {
       return cy.getCookie("session-token").should("exist");
   }})
